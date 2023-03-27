65faf93e9cb75e5b1db0ed1caecacc90599574b1
jjw <2371671097@qq.com>
2023-03-27 19:54:04 +0800

    Alibaba图标

--- frontcode/src/App.vue
+++ frontcode/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div></div>
+  <div>
+    <span>app</span>
+    <span class="iconfont icon-ercikaifa"> </span>
+  </div>
 </template>
 <script>
 </script>
--- frontcode/src/router/index.js
+++ frontcode/src/router/index.js
@@ -1,27 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import App from '../App.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: App
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../App.vue')
-  }
 ]
 
 const router = new VueRouter({
   routes
 })
-
 export default router
