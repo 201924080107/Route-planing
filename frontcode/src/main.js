8984ffdb31a86e90b781e6aed1e6fcdcb9171925
jjw <2371671097@qq.com>
2023-03-27 18:39:00 +0800

    ok

--- frontcode/src/main.js
+++ frontcode/src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import router from './router'
+import store from './store'
 import './plugins/element.js'
 
-Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
